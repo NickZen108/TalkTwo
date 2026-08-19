5b5a3e7f0c77795088f90b2171c8757499dcf175
NickZen108 <nicolaipetersen108@gmail.com>
2026-08-19 21:43:20 +0200

    Add message filter types

---
+++ src/filter/types.ts
@@ -0,0 +1,27 @@
+export type RiskLevel = 'green' | 'yellow' | 'red';
+
+export type FilterReasonCode =
+  | 'too_long'
+  | 'exclamation_mark'
+  | 'emoji'
+  | 'profanity'
+  | 'generalisation'
+  | 'personal_attack'
+  | 'accusatory_question'
+  | 'emotion_dumping'
+  | 'criticism'
+  | 'caps_lock';
+
+export interface FilterReason {
+  code: FilterReasonCode;
+  title: string;
+  explanation: string;
+  suggestion: string;
+  matchedText?: string;
+}
+
+export interface FilterResult {
+  level: RiskLevel;
+  canSend: boolean;
+  reasons: FilterReason[];
+}
